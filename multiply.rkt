3978b8b1d6dc651f1439a6a6348dee9036a5f538
arushi-bhatt <arushi131.bhatt@gmail.com>
2016-06-28 21:23:11 +0530

    all values stored
    all values are stored and list ref. is made

--- multiply.rkt
+++ multiply.rkt
@@ -1,32 +1,42 @@
 ;This program provides practice session for multiplication using speed mathematics skills. 
 #lang racket
+
 ;variable for storing score
 (define score 0)
-(define lst  (list  '0))
+(define lst  (list ))
+(define wrng (list))
 (define i 0)
 
 ;10 sets of no. will be printed
 (for ([i (in-range  1 6)]) 
   ;random nos. within range of 1-100 will be generated
   (define-values (p q) (values (random 1 100) (random 1 100)))
-  (set! lst (append lst (list p)(list q)))
+  
   
   ;print the nos.
   (printf "(~a) ~a * ~a \n"i p q)
   ;gets answer from user and store it in the variable x
   
   (define x (read))
+  (set! lst (append lst (list p)(list q)(list x)))
   ; checks for the correct answer and increment the value of 'score' if input ans is true  
-  (cond [(equal? x (* p q )) (printf "true :) \n") +(set! score (add1 score))] 
+  (cond [(equal? x (* p q )) (set! score (add1 score))] 
         ;if input answer is wrong then print false
-        [else (printf "false :( \n")]  
+        [else  (set! wrng (append wrng (list (* i 3))))]  
         )
   )
-(printf "\n****  your score = ~a/5" score)  ;print your score
 
-(for ([i (in-range  1 12)])
-  (printf "~a * ~a \n "
-          (list-ref lst(+ i 1))
-          (list-ref lst(+ i 2))
+(printf "\n****  your score = ~a/5 \n" score)  ;print your score 
+(printf "\n****  correct = ~a \n" score )
+(printf "\n****  incorrect = ~a  \n" (- 5 score))
+(cond [(equal? (- 5 score) 0) (printf "*** Great!! all correct  *** ") ] 
+       
+        [else  (for ([i (in-range  0 (length (wrng)) )])
+  (printf "~a * ~a  = ~a  ||  ~a   \n "
+          (list-ref lst (list-ref wrng i ) )
+          (list-ref lst(+ (list-ref wrng i ) 1))
+          (*  (list-ref lst (list-ref wrng i ) ) (list-ref lst(+ (list-ref wrng i ) 1))
           )
-  )
+          (list-ref lst(+ (list-ref wrng i ) 2)))
+  )]  
+        )
